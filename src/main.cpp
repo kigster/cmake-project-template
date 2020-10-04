9f3bf4604e447f986efb2da0f004e60573bff216
Samyak Padwekar <samyaksam007@gmail.com>
2020-10-04 11:06:52 +0530

    Modified main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <division.h>
 
+using namespace std;
+
 static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
 static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
 
 int main(int argc, const char *argv[]) {
   Fraction f;
 
-  std::cout << HEADER;
+  cout << HEADER;
 
   // ensure the correct number of parameters are used.
   if (argc < 3) {
-    std::cout << USAGE;
+    cout << USAGE;
     return 1;
   }
 
@@ -21,10 +23,10 @@ int main(int argc, const char *argv[]) {
   try {
     DivisionResult r = d.divide();
 
-    std::cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
-    std::cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
+    cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
+    cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
   } catch (DivisionByZero) {
-    std::cout << "Can not divide by zero, Homer. Sober up!\n";
+    cout << "Can not divide by zero, Homer. Sober up!\n";
   }
   return 0;
 }
